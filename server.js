a645f30a5828d82f7bed708865f88e26d0369330
Alpr <40207379+misteralipour@users.noreply.github.com>
2020-12-14 10:29:51 +0330

    base code

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 8080;
-
-app.use(express.static('./www/'));
-app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'www/' });
-});
-app.listen(process.env.PORT || port);
