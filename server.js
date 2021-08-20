008a63f98bd0ecb87bcc591c0ff82d1c15c5b80a
Ali Zareshahi <a.zareshahi1377@gmail.com>
2021-08-20 18:57:34 +0430

    build(firebase): add firebase to project with https://hodhod.web.app

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.use(express.static('./www/'));
+app.use(express.static('./public/'));
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'www/' });
+  res.sendFile('index.html', { root: 'public/' });
 });
 app.listen(process.env.PORT || port);
